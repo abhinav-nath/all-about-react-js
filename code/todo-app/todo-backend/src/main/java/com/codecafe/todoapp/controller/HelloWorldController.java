22df54ae2962e63d4bebbe724f976b3e2e144efd
Abhinav Nath <abhinavnath@ymail.com>
2022-01-16 02:15:39 +0530

    Handle CORS and make a successful call to backend

--- code/todo-app/todo-backend/src/main/java/com/codecafe/todoapp/controller/HelloWorldController.java
+++ code/todo-app/todo-backend/src/main/java/com/codecafe/todoapp/controller/HelloWorldController.java
@@ -2,11 +2,13 @@ package com.codecafe.todoapp.controller;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("http://localhost:8800")
 public class HelloWorldController {
 
     @GetMapping("/hello-world")
--- code/todo-app/todo-frontend/src/components/HelloWorld.js
+++ code/todo-app/todo-frontend/src/components/HelloWorld.js
@@ -2,22 +2,36 @@ import React, { Component } from "react";
 import HelloWorldService from "../api/todo/HelloWorldService";
 
 class HelloWorld extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: ""
+    };
+  }
+
   render() {
     return (
       <div className="card w-25 bg-light rounded mx-auto d-block">
         <div className="card-body">
-          <p>Click below button to get a message from the b ackend</p>
+          <p>Click below button to get a message from the backend</p>
           <button onClick={this.retrieveHello} className="btn btn-outline-success">
             Get Hello
           </button>
+          <p>{this.state.message}</p>
         </div>
       </div>
     );
   }
 
   retrieveHello = () => {
-    HelloWorldService.execute().then((response) => console.log(response));
+    HelloWorldService.execute().then((response) =>
+      this.handleSuccesfulResponse(response)
+    );
     //.catch();
+  };
+
+  handleSuccesfulResponse = (response) => {
+    this.setState({ message: response.data });
   };
 }
 
