a77ad9c71cb9129af7d35c62124cb536a15f753c
Abhinav Nath <abhinavnath@ymail.com>
2022-01-15 22:12:29 +0530

    Setup basic HTML layout of Todos List

--- code/todo-app/todo-frontend/src/components/ListTodos.js
+++ code/todo-app/todo-frontend/src/components/ListTodos.js
@@ -2,8 +2,36 @@ import React, { Component } from "react";
 import "./ListTodos.css";
 
 class ListTodos extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todo: {
+        id: 1,
+        description: "Learn React"
+      }
+    };
+  }
+
   render() {
-    return <div className="ListTodos">List Todos</div>;
+    return (
+      <div className="ListTodos">
+        <h1>List Todos</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>id</th>
+              <th>description</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{this.state.todo.id}</td>
+              <td>{this.state.todo.description}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
   }
 }
 
