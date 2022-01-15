80ec74b4d3c808899b33767c6683f1f34f9b14b6
Abhinav Nath <abhinavnath@ymail.com>
2022-01-16 01:20:09 +0530

    Add small section in the page for hello world api

--- code/todo-app/todo-frontend/src/components/TodoApp.js
+++ code/todo-app/todo-frontend/src/components/TodoApp.js
@@ -1,15 +1,30 @@
 import React, { Component } from "react";
 import ListTodos from "./ListTodos";
-import "./TodoApp.css";
 
 class TodoApp extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <div className="TodoApp">
         <ListTodos />
+        <div className="card w-25 bg-light rounded mx-auto d-block">
+          <div className="card-body">
+            <p>Click below button to get a message from the backend</p>
+            <button onClick={this.retrieveHello} className="btn btn-outline-success">
+              Get Hello
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
+
+  retrieveHello = () => {
+    console.log("retrieve clicked!");
+  };
 }
 
 export default TodoApp;
