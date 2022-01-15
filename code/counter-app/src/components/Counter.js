d7f91d3c0f1dea0b107ba6d86ba00c46e0a7c2d9
Abhinav Nath <abhinavnath@ymail.com>
2022-01-15 14:51:22 +0530

    Increment counter using state

--- code/counter-app/src/components/Counter.js
+++ code/counter-app/src/components/Counter.js
@@ -2,17 +2,35 @@ import React, { Component } from "react";
 import "./Counter.css";
 
 class Counter extends Component {
+  // Define the initial state in the constructor
+  // state => counter 0
+  constructor() {
+    super(); // mandatory
+
+    this.state = {
+      counter: 0
+    };
+
+    // to make 'this' available to the increment function
+    // we need to bind 'this' to the increment function
+    this.increment = this.increment.bind(this);
+  }
+
   render() {
     return (
       <div className="Counter">
         <button onClick={this.increment}>+1</button>
-        <span className="count">0</span>
+        <span className="count">{this.state.counter}</span>
       </div>
     );
   }
 
+  // Update the state by using setState()
+  // counter++
   increment() {
-    console.log("incremented");
+    this.setState({
+      counter: this.state.counter + 1,
+    });
   }
 }
 
