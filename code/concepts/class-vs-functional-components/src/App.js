33382031b360f41eba5bc77859faa43e1d665cd1
Abhinav Nath <abhinavnath@ymail.com>
2022-02-10 23:58:01 +0530

    Create new project class-vs-functional-components

---
+++ code/concepts/class-vs-functional-components/src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import ClassComponent from "./components/ClassComponent";
+
+export default function App() {
+  return (
+    <div className="App">
+      <ClassComponent />
+    </div>
+  );
+}
--- code/todo-app/todo-frontend/src/components/HelloWorld.js
+++ code/todo-app/todo-frontend/src/components/HelloWorld.js
@@ -8,7 +8,6 @@ class HelloWorld extends Component {
       message: ""
     };
   }
-()
   render() {
     return (
       <div className="card w-25 bg-light rounded mx-auto d-block">
