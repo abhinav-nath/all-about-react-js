9bfedfdac8495c95bd436337feaad2677fade0c2
Abhinav Nath <abhinavnath@ymail.com>
2022-04-30 00:08:28 +0530

    Detect navigation by events

--- code/widgets/src/components/Route.js
+++ code/widgets/src/components/Route.js
@@ -1,4 +1,19 @@
+import { useEffect } from "react";
+
 const Route = ({ path, children }) => {
+  useEffect(() => {
+    const onLocationChange = () => {
+      console.log("Location Change");
+    };
+
+    window.addEventListener("popstate", onLocationChange);
+
+    // clean-up
+    return () => {
+      window.removeEventListener("popstate", onLocationChange);
+    };
+  }, []); // run only when this component is first rendered
+
   return window.location.pathname === path ? children : null;
 };
 
