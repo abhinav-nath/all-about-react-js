db343066a0eacf06f69a12ba95d8fc3e204d9ab7
Abhinav Nath <abhinavnath@ymail.com>
2022-04-30 00:16:02 +0530

    Handle cmd+click (Open the link in new tab)

--- code/widgets/src/components/Link.js
+++ code/widgets/src/components/Link.js
@@ -2,6 +2,10 @@ import React from "react";
 
 const Link = ({ className, href, children }) => {
   const onClick = (event) => {
+    if (event.metaKey || event.ctrlKey) {
+      return;
+    }
+
     event.preventDefault(); // do not do a full page reload
     window.history.pushState({}, "", href);
 
