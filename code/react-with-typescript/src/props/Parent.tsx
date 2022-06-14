51520502a31b2157c85cfac62feee6a274d10dd9
Abhinav Nath <abhinavnath@ymail.com>
2022-06-15 00:10:30 +0530

    Add a callback method in props

--- code/react-with-typescript/src/props/Parent.tsx
+++ code/react-with-typescript/src/props/Parent.tsx
@@ -1,7 +1,7 @@
 import { Child } from './Child';
 
 const Parent = () => {
-    return <Child color="Blue" />
+    return <Child color="Blue" onClick={() => console.log('Clicked!')} />
 };
 
 export default Parent;
