d1198f81fbf9f66ba53f62031adff52daed221d9
Abhinav Nath <abhinavnath@ymail.com>
2022-11-23 22:45:22 +0530

    Remove redux-with-typescript

--- code/redux-with-typescript/src/state/store.ts
+++
@@ -1,6 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-import { configureStore } from "@reduxjs/toolkit";
-import thunk from "redux-thunk";
-import reducers from "./reducers";
-
-export const store = createStore(reducers, {}, applyMiddleware(thunk));
