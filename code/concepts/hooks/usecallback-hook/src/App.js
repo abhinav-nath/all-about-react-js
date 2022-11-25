aab3da70669578a0f4fa3acfaa5dc015cc8ec3f7
Abhinav Nath <abhinavnath@ymail.com>
2022-11-25 23:49:12 +0530

    Add code for useCallback hook

---
+++ code/concepts/hooks/usecallback-hook/src/App.js
@@ -0,0 +1,34 @@
+import "./App.css";
+import List from "./components/List";
+
+import React, { useState, useCallback } from "react";
+
+const App = () => {
+  const [number, setNumber] = useState(1);
+  const [dark, setDark] = useState(false);
+
+  const getItems = useCallback(() => {
+    return [number, number + 1, number + 2];
+  }, [number]);
+
+  const theme = {
+    backgroundColor: dark ? "#333" : "#FFF",
+    color: dark ? "#FFF" : "#333"
+  };
+
+  return (
+    <div style={theme}>
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => setNumber(parseInt(e.target.value))}
+      />
+      <button onClick={() => setDark((prevDark) => !prevDark)}>
+        Toggle Theme
+      </button>
+      <List getItems={getItems} />
+    </div>
+  );
+};
+
+export default App;
